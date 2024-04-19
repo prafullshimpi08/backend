fbc308f989515f016620afa53917369d5cf362b7
prafull <prafullshimpi2829@.com>
2024-04-19 17:23:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+  res.send('prafulldotcom')
+})
+
+app.get('/youtube', (req,res) => {
+   res.send('tejaswini shimpi')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
